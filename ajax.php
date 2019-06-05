e6cffab5185053e830ddbca41863c5ab906855b2
nipunadodan <nipunadodan@users.noreply.github.com>
2019-06-05 11:19:52 +0530

    init1

---
+++ ajax.php
@@ -0,0 +1,14 @@
+<?php
+include_once('config.php');
+include_once(INC_PATH.'dbconnect.php');
+include_once(FUNCTIONS);
+if(isset($_GET['process']) && $_GET['process'] !== ''){
+    if(file_exists(MODULE_PATH.$_GET['process'].'.php')){
+        include_once(MODULE_PATH.$_GET['process'].'.php');
+    }else{
+        echo '404: File '.$_GET['process'].' not found';
+    }
+}else{
+    echo '404: Process not Found';
+}
+?>
