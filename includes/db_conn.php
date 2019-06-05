6fd6c5c09725bb5f9aa9c37fa78a9826dc0c1e9e
nipunadodan <nipunadodan@users.noreply.github.com>
2019-06-05 11:22:28 +0530

    Add files via upload

--- includes/db_conn.php
+++ includes/db_conn.php
@@ -1,1 +1,13 @@
 <?php
+// Using Medoo namespace
+use Medoo\Medoo;
+
+$database = new Medoo([
+// required
+'database_type' => 'mysql',
+'database_name' => DB,
+'server' => DB_HOST,
+'username' => DB_USER,
+'password' => DB_PASSWORD,
+'prefix' => DB_PREFIX,
+]);
