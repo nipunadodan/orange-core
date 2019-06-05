4d8c4e5058786e742c479962952abdf64e9037e4
Nipuna Dodantenna <nipunadodan@gmail.com>
2019-06-05 11:26:35 +0530

    init

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+define('CORE_BASE', dirname(realpath(__FILE__))."/");
+define("CORE_URL", SITE_URL.'vendor/nipunadodan/orange-core/');
+define("CORE_INC_PATH", CORE_BASE."includes/");
+define("CORE_INC_URL", CORE_BASE."includes/");
+define("CORE_INC_JS_URL", CORE_BASE."includes/js/");
+define("CORE_FUNCTIONS", CORE_BASE."functions.php");
